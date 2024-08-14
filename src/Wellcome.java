14c8a9ca2276e665ab44a498f46c19f9b76cf7d8
Admin <23699841.phuc@student.iuh.edu.vn>
2024-08-14 19:14:18 +0700

    hophuc

---
+++ src/Wellcome.java
@@ -0,0 +1,6 @@
+public class Wellcome {
+    public static void main(String[] args) {
+        System.out.println(" Hello Quang Phuc");
+        System.out.println(" Wellcome to the DHDTVT19A!");
+    }
+}
